000a15fd16a6ed2e7d341c328c0d79d992fd3ede
quadri <quadri@adisaquadri234@yahoo.com>
2020-05-29 16:00:07 +0100

    rounding up my project

---
+++ build/precache-manifest.b0fa5faf399a1623f25e2b147ca771a5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcad854cf12094985cfa46fcea7b910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49f54fc82b4b702d598",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "619b86ad884b874117e6",
+    "url": "/static/css/main.098f0fea.chunk.css"
+  },
+  {
+    "revision": "f49f54fc82b4b702d598",
+    "url": "/static/js/2.30816d98.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/static/js/2.30816d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619b86ad884b874117e6",
+    "url": "/static/js/main.48af7b2d.chunk.js"
+  },
+  {
+    "revision": "e225e0d4e9eb7f978c85",
+    "url": "/static/js/runtime-main.5c9434c7.js"
+  },
+  {
+    "revision": "95726e8d1792ee97f125419955cf5e7d",
+    "url": "/static/media/Chef.95726e8d.png"
+  },
+  {
+    "revision": "0b11d7ba692acafe83f5b91246881cf9",
+    "url": "/static/media/about-banner.0b11d7ba.jpg"
+  },
+  {
+    "revision": "45284815c9ba9078bd9028df0a1a47b7",
+    "url": "/static/media/booking.45284815.jpg"
+  },
+  {
+    "revision": "53404e0e1aef220a30c7842a10b3fb08",
+    "url": "/static/media/chef_1.53404e0e.jpg"
+  },
+  {
+    "revision": "1ba52112244dc0c80a708dbcd3cc0973",
+    "url": "/static/media/chefe.1ba52112.png"
+  },
+  {
+    "revision": "a8c4cc2a81ae55315d9fc1c8efe72f4e",
+    "url": "/static/media/contact-banner.a8c4cc2a.jpg"
+  },
+  {
+    "revision": "663ca709e0d52b034ce981c198f45f51",
+    "url": "/static/media/firstslide.663ca709.jpg"
+  },
+  {
+    "revision": "1506cd4edcf1668b2316762b02fb19c7",
+    "url": "/static/media/manna.1506cd4e.jpg"
+  },
+  {
+    "revision": "88ba49373e435e4c6b01e3d323dd44c2",
+    "url": "/static/media/secondslide.88ba4937.jpg"
+  },
+  {
+    "revision": "bfc7dd792f88b2d7e31b960bc80cf873",
+    "url": "/static/media/service-banner.bfc7dd79.jpg"
+  },
+  {
+    "revision": "e3ba5288100aa0830889437ffc9b4d05",
+    "url": "/static/media/side-img.e3ba5288.jpg"
+  },
+  {
+    "revision": "0b62c53d4d5502b21c5eeee381e0b4f6",
+    "url": "/static/media/thirdslide.0b62c53d.jpg"
+  }
+]);
